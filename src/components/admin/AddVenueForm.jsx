dd7eade49027c4dee10cc15790aa4b2d31f01463
prasadchowdary9 <prasadchowdary983@gmail.com>
2025-04-03 21:13:13 +0530

    updated addvenue and backend integration

--- src/components/admin/AddVenueForm.jsx
+++ src/components/admin/AddVenueForm.jsx
@@ -305,6 +305,20 @@
 // export default AddVenueForm;
  
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 import React, { useState } from 'react';
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
